3389b4636c3ad5ee258c770edb725863dc7bd82e
Dylan Romkey <Dylan.Romkey@corp.eastlink.ca>
2017-06-07 13:22:59 -0300

    login done

--- www/js/view/AuthView.js
+++ www/js/view/AuthView.js
@@ -3,10 +3,11 @@ define([
   'underscore',
   'jquery',
   'backbone',
+  'bootstrap',
   'tool/token',
   'tool/msg',
   'text!/templates/tplLogin.html'
-], function(_,$,Backbone,token,msg,template) {
+], function(_,$,Backbone,bootstrap,token,msg,template) {
 
 
   var auth = Backbone.View.extend({
@@ -16,22 +17,17 @@ define([
         this.$el.html(this.template());
         var msgCode = msg.getMsg();
         if (msgCode){
-          switch(msgCode){
-            case 1:
-              this.$el.find('#errorMsg').html("<h3>Something went wrong, please login again</h3>");
-              break;
-            case 2:
-              this.$el.find('#errorMsg').html("<p><i>You have been logged out</i></p>");
-              break;
-          };
+          this.writeMsg(msgCode);
           window.sessionStorage.clear();
         };
         return this;
       },
       getAuth: function(){
+        console.log("in get auth");
         var pass = this.$el.find('#password').val();
         var name = this.$el.find('#username').val();
         var that = this;
+        console.log(pass, name);
         $.ajax({
             url:"/v1/auth",
             method: "POST",
@@ -50,19 +46,42 @@ define([
               if (token.setToken(data.token)){
                 window.location = 'index.html';
               }else{
-                that.$el.find('#errorMsg').html("<h3>Problem storing your authorization</h3>");
+                msg.setMsg(4);
+                console.log("token fail");
               };
               return false;
             } else {
-                that.$el.find('#errorMsg').html("<h3>"+data.message+"</h3>");
+              msg.setMsg(4);
+              console.log(data.message);
             }
         }).fail(function(jqXHR, textStatus, errorThrown){
-            console.log('ajax ', errorThrown);
-            that.$el.find('#errorMsg').html("<h3>"+errorThrown+"</h3>");
+            console.log('ajax fail'+ errorThrown);
+            msg.setMsg(3);
+            // that.$el.find('#errorMsg').html("<h3>"+errorThrown+"</h3>");
         });
       },
       events: {
         'click #button-login':'getAuth'
+      },
+      writeMsg: function(er){
+        console.log("in write error:",er);
+        switch(er){
+          case '1':
+            this.$el.find('#errorMsg').html("<h3>Something went wrong, please login again</h3>");
+            break;
+          case '2':
+            this.$el.find('#errorMsg').html("<p><i>You have been logged out</i></p>");
+            break;
+          case '3':
+            this.$el.find('#errorMsg').html("<h3>Can not find a user with that username and password</h3>");
+            break;
+          case '4':
+            this.$el.find('#errorMsg').html("<h3>Problem storing your authorization</h3>");
+            break;
+          default:
+            this.$el.find('#errorMsg').html("<h3>"+er+"</h3>");
+            break;
+        };
       }
   });
   return auth;
--- www/loginConfig.js
+++ www/loginConfig.js
@@ -2,11 +2,17 @@ require.config({
   baseUrl: "/",
   paths: {
     "jquery":"lib/jquery-3.2.1.min",
+    "bootstrap":"lib/bootstrap/js/bootstrap",
     "underscore":"lib/underscore-amd",
     "backbone":"lib/backbone-amd",
     "text":"lib/text",
     tool : "js/tool",
     view : "js/view"
+  },
+  shim: {
+    'bootstrap': {
+        deps: ['jquery']
+    }
   }
 })
 
